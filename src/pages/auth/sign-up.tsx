91d2093f1cde7c507e5299319f349f3d392d0519
feliperbdantas <feliperbdantas@outlook.com>
2024-05-28 23:51:57 -0300

    feat: impletentation of the sign up and sign in page

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -8,34 +8,34 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-const signInForm = z.object({
+const signUpForm = z.object({
   email: z
     .string()
     .min(1, { message: 'E-mail obrigatório' })
     .email({ message: 'E-mail inválido' }),
 })
 
-type SignInForm = z.infer<typeof signInForm>
+type SignUpForm = z.infer<typeof signUpForm>
 
-export function SignIn() {
+export function SignUp() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>()
+  } = useForm<SignUpForm>()
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignUp(data: SignUpForm) {
     try {
       console.log(data)
 
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
-      toast.success('Enviamos um link de autenticação para seu e-mail.', {
-        action: {
-          label: 'Reenviar',
-          onClick: () => handleSignIn(data),
-        },
-      })
+      // toast.success('Enviamos um link de autenticação para seu e-mail.', {
+      //   action: {
+      //     label: 'Reenviar',
+      //     onClick: () => handleSignUp(data),
+      //   },
+      // })
     } catch {
       toast.error('Credenciais inválidas.')
     }
@@ -43,32 +43,32 @@ export function SignIn() {
 
   return (
     <>
-      <Helmet title="Login" />
+      <Helmet title="Cadastro" />
 
       <div className="p-8">
         <Button variant="ghost" asChild className="absolute right-8 top-8">
-          <Link to="/sign-up">Novo estabelecimento</Link>
+          <Link to="/sign-in">Fazer login</Link>
         </Button>
 
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Acessar painel
+              Criar conta grátis
             </h1>
 
             <p className="text-sm text-muted-foreground">
-              Acompanhe suas vendas pelo painel do parceiro!
+              Seja um parceiro e comece suas vendas!
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignUp)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
               <Input id="email" type="email" {...register('email')} />
             </div>
 
             <Button disabled={isSubmitting} className="w-full" type="submit">
-              Acessar painel
+              Finalizar cadastro
             </Button>
           </form>
         </div>
