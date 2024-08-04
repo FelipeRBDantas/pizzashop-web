92853349d516c0bb74ea9c113d8e330eb04933ed
feliperbdantas <feliperbdantas@outlook.com>
2024-08-04 20:50:44 -0300

    test: Tests and Closing - Registration and Dashboard Mocks - Dashboard

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,10 +2,11 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
-import { getDayOrdersAmountMock } from './get-day-orders-amount'
-import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
-import { getMonthOrdersAmountMock } from './get-month-orders-amount'
-import { getMonthRevenueMock } from './get-month-revenue'
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 
@@ -16,6 +17,7 @@ export const worker = setupWorker(
   getMonthOrdersAmountMock,
   getMonthCanceledOrdersAmountMock,
   getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
 )
 
 export async function enableMSW() {
